ea64b87b232808cab46af0554245a9ca6c502cfb
SteveGremory <stevegremory2006@gmail.com>
2022-05-11 21:46:16 +0530

    Initial Committ

---
+++ source/generalstuff/general.c
@@ -0,0 +1,8 @@
+#include "general.h"
+#include "stdio.h"
+
+int SayHello()
+{
+    return printf("Hello!\n");
+    ;
+}
---
+++ source/main.c
@@ -0,0 +1,7 @@
+#include "generalstuff/general.h"
+
+int main()
+{
+    SayHello();
+    return 0;
+}
